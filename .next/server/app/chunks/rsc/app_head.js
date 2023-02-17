94255b4aeff1ee1e16a5efbfc25213754b921c0a
Ha Anna <83631167+its-haanna@users.noreply.github.com>
2023-02-17 20:18:27 +0900

    Delete .next directory

--- .next/server/app/chunks/rsc/app_head.js
+++
@@ -1,9 +0,0 @@
-(self.TURBOPACK = self.TURBOPACK || []).push(["chunks/rsc/app_head.js", {
-
-"[project-with-next]/app/head.js (ecmascript, rsc)": (({ r: __turbopack_require__, x: __turbopack_external_require__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_cjs__, p: process, g: global, __dirname }) => (() => {
-
-const e = new Error("Could not parse module '[project-with-next]/app/head.js'");
-e.code = 'MODULE_UNPARSEABLE';
-throw e;
-})()),
-}]);
--- .next/server/flight-server-css-manifest.js
+++
@@ -1,5 +0,0 @@
-self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "/Users/ha.anna/Desktop/coding/EDU_nextjs_tutorial/app/page": []
-  }
-}
